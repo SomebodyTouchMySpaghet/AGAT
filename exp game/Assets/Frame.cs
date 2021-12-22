ba0639ca6832c94196eb1e332ad38cac73c599f5
TheCringiestCoder <73342382+SomebodyTouchMySpaghet@users.noreply.github.com>
2021-12-22 16:41:38 +0100

    FPS Test

---
+++ exp game/Assets/Frame.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frame : MonoBehaviour {
+    
+    int myInt = 6;
+    
+    void Update () {
+        print(myInt);
+     
+    }
+
+}
